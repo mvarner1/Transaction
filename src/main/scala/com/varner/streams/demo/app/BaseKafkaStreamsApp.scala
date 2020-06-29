9fe8e1021f5d0f0ab8c0616aab227e6448eec651
monowaralam <62983167+monowaralam@users.noreply.github.com>
2020-06-29 12:57:05 +0600

    Default key serde properties removed

--- src/main/scala/com/varner/streams/demo/app/BaseKafkaStreamsApp.scala
+++ src/main/scala/com/varner/streams/demo/app/BaseKafkaStreamsApp.scala
@@ -17,8 +17,6 @@ trait BaseKafkaStreamsApp {
     val config = new Properties()
     config.put(StreamsConfig.APPLICATION_ID_CONFIG, applicationId)
     config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092")
-    config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String.getClass)
-    config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String.getClass)
 
     val builder = new StreamsBuilder
     val topology = createTopology(builder)
